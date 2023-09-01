e2d15fd345b93e1c5319811e050b4931bb80d5ed
nagarajugudipati1@gmail.com <nagarajugudipati1@gmail.com>
2023-09-01 19:31:44 +0530

    P2-AssHTMLForms

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HTMLForms.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
